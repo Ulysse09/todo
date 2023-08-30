1154cebebc1f8bd426777e48ec77cb7b66e3c2d8
ulysse09 <upkabalisa@gmail.com>
2023-08-30 18:21:13 +0200

    first commit

--- main.js
+++ main.js
@@ -4,16 +4,33 @@ let toDoContainer = document.getElementById('toDoContainer');
 
 addT.addEventListener('click',function(){
      let  paragraph = document.createElement('p');
+     let button = document.createElement('button');
+     let button2 = document.createElement('button');
+     let tdcontainer1 = document.createElement('div');
+     
+
      paragraph.innerText = inputField.value;
+     button.innerText = 'Delete';
+     button2.innerText = "Completed";
+
      paragraph.classList.add('font-semibold','text-2xl');
-     toDoContainer.appendChild(paragraph);
-     inputField.value = ''
-     paragraph.addEventListener('click',function() {
+     button.classList.add('font-semibold','text-white','bg-red-200','px-4');
+     button2.classList.add('font-semibold','text-white','bg-amber-200','px-4');
+     tdcontainer1.classList.add('flex', 'flex-row' ,'justify-between','gap-4','p-4');
+
+     toDoContainer.appendChild(tdcontainer1);
+     tdcontainer1.appendChild(paragraph);
+     tdcontainer1.appendChild(button);
+     tdcontainer1.appendChild(button2);
+      inputField.value = ''
+     
+     
+      button2.addEventListener('click',function() {
         paragraph.style.textDecoration = 'line-through'
      }
      )
-     paragraph.addEventListener('dblclick',function (){
-        toDoContainer.removeChild(paragraph);
+     button.addEventListener('click',function (){
+        toDoContainer.removeChild(tdcontainer1);
      })
 console.log('yoo');
 
